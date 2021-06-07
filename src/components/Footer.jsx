fb57d9b096566de940d69f8b8845557a6707b60e
David <69402666+Social713@users.noreply.github.com>
2021-06-07 19:28:02 -0400

    Add files via upload
    - Updated the footer design and text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,37 +1,72 @@
-import React from 'react';
-import styled from 'styled-components';
-// import SiLinkedin from 'react-icons/si';
+import React from "react";
+import styled from "styled-components";
+import { SiLinkedin } from "react-icons/si";
+import {FaReact} from 'react-icons/fa';
 
 const Foot = styled.div`
-    height: 75px;
-    padding-top: 10px;
-    display: flex;
-    justify-content: center;
-    z-index: 100;
-    position: absolute;
-    width: 100%;
-    bottom: 0;
-    left: 0;
-    margin-top: 5%;
-    background-color: black;
+  height: 125px;
+  padding-top: 10px;
+  display: flex;
+  justify-content: center;
+  z-index: 100;
+  position: absolute;
+  width: 100%;
+  bottom: 0;
+  left: 0;
+  margin-top: 5%;
+  background-color: black;
 
-    p{
-        padding-top: 20px;
-        color: grey;
-        font-size: 14px;
-    }
+  pre {
+    padding-top: 10px;
+    color: grey;
+    font-size: 14px;
+  }
+`;
+
+const Text = styled.div`
+  position: relative;
+  height: 100%;
+  width: 100%;
+  text-align: center;
+`;
+
+const LinkedInIcon = styled(SiLinkedin)`
+  height: 25px;
+  width: 25px;
+  color: gray;
+  position: relative;
+  justify-content: center;
+  text-align: center;
+  margin-top: 15px;
+
+  &:hover {
+    color: skyblue;
+  }
+`;
+
+const FooterReactIcon = styled(FaReact)`
+  height: 14px;
+  width: 14px;
 `;
 
 export const Footer = () => {
-    let date = new Date().getFullYear()
-    return (
-        <Foot>
-            <p>DAVID ROGERS © {date}</p>
-            {/* <div>
-            <p>made with React</p>
-            </div> */}
-        </Foot>
-    )
-}
-
-
+  let date = new Date().getFullYear();
+  return (
+    <Foot>
+      <Text>
+        <a
+          href={"https://www.linkedin.com/in/david-rogers-8828941b4/"}
+          target={"_blank"}
+          rel={"noopener noreferrer"}
+          >
+          <LinkedInIcon />
+        </a>
+        <pre>
+          DAVID ROGERS © {date}
+          <br></br>
+          made with React <FooterReactIcon />
+        </pre>
+      </Text>
+    </Foot>
+  );
+};
