35b6f8937727f7f73cb2403cbf69ee6716f2381b
David <69402666+Social713@users.noreply.github.com>
2021-06-21 22:34:10 -0400

    Add files via upload
    Edited the styles for the "About Me" header

--- src/styles/InfoSectionStyles.jsx
+++ src/styles/InfoSectionStyles.jsx
@@ -1,23 +1,29 @@
-import styled from 'styled-components';
-import {FaReact, FaNodeJs} from 'react-icons/fa';
-import {SiJavascript, SiCss3, SiMongodb, SiJquery} from 'react-icons/si';
-import {AiOutlineHtml5} from 'react-icons/ai';
+import styled from "styled-components";
+import { FaReact, FaNodeJs } from "react-icons/fa";
+import { SiJavascript, SiCss3, SiMongodb, SiJquery } from "react-icons/si";
+import { AiOutlineHtml5 } from "react-icons/ai";
 
 export const Section = styled.section`
-    width: 100%;
-    height: 100%;
-  
-    `;
+  width: 100%;
+  height: 100%;
+  h1{
+        margin-bottom: 1rem;
+        font-size: clamp(1.5rem, 6vw, 2rem);
+        padding-top: 5%;
+        margin-top: 1%;
+        text-align: center;
+    }
+`;
 
 export const Container = styled.div`
-    padding: 3% 3% 2% 0;
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    grid-template-rows: 800px;
+  padding: 3% 3% 2% 0;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: 800px;
 
-    @media screen and (max-width: 768px){
-        grid-template-columns: 1fr;
-}
+  @media screen and (max-width: 768px) {
+    grid-template-columns: 1fr;
+  }
 `;
 
 export const TextColumn = styled.div`
@@ -27,7 +33,7 @@ export const TextColumn = styled.div`
     align-items: center;
     line-height: 1.4;
     padding: 1rem, 2rem;
-    order: ${({reverse}) => (reverse ? '2' : '1')};
+    order: ${({ reverse }) => (reverse ? "2" : "1")};
 
     h1{
         margin-bottom: 1rem;
@@ -49,26 +55,27 @@ export const TextColumn = styled.div`
 `;
 
 export const ImageColumn = styled.div`
-    padding: 1rem 2rem;
-    order: ${({reverse}) => (reverse ? '1' : '2')};
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  padding: 1rem 2rem;
+  order: ${({ reverse }) => (reverse ? "1" : "2")};
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
-    @media screen and (max-width: 768px){
-        order: ${({reverse}) => (reverse ? '2' : '1')};
-}
+  @media screen and (max-width: 768px) {
+    order: ${({ reverse }) => (reverse ? "2" : "1")};
+  }
 
-img{
-    width: 50%;
-    height: 50%;
+  img {
+    width: 20vh;
+    height: 20vh;
     object-fit: cover;
-    border-radius: 25px;
-    @media screen and (max-width: 768px){
-        width: 90%;
-        height: 90%;
+    border-radius: 100%;
+    @media screen and (max-width: 768px) {
+      width: 90%;
+      height: 90%;
     }
-}`;
+  }
+`;
 
 export const LogoTitle = styled.div`
     width : 100%;
@@ -83,48 +90,47 @@ export const LogoTitle = styled.div`
 `;
 
 export const ReactIcon = styled(FaReact)`
-    height: 80px;
-    width: 80px;
-    margin: 2%;
+  height: 80px;
+  width: 80px;
+  margin: 2%;
 `;
 
 export const JsIcon = styled(SiJavascript)`
-    height: 80px;
-    width: 80px;
-    margin: 2%;
+  height: 80px;
+  width: 80px;
+  margin: 2%;
 `;
 
 export const HtmlIcon = styled(AiOutlineHtml5)`
-    height: 80px;
-    width: 80px;
-    margin: 2%;
+  height: 80px;
+  width: 80px;
+  margin: 2%;
 `;
 
 export const CssIcon = styled(SiCss3)`
-    height: 80px;
-    width: 80px;
-    margin: 2%;
+  height: 80px;
+  width: 80px;
+  margin: 2%;
 `;
 
 export const NodeIcon = styled(FaNodeJs)`
-    height: 80px;
-    width: 80px;
-    margin: 2%;
+  height: 80px;
+  width: 80px;
+  margin: 2%;
 `;
 
 export const MongoIcon = styled(SiMongodb)`
-    height: 80px;
-    width: 80px;
-    margin: 2%;
+  height: 80px;
+  width: 80px;
+  margin: 2%;
 `;
 
 export const JQueryIcon = styled(SiJquery)`
-    height: 80px;
-    width: 80px;
-    margin: 2%;
+  height: 80px;
+  width: 80px;
+  margin: 2%;
 `;
 
-export const TextWrapper =styled.div`
-    width : 100%; 
-
+export const TextWrapper = styled.div`
+  width: 100%;
 `;
